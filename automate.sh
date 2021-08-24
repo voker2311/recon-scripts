0a461448c6667ba3769cc528ae1526137510dfe7
Akshay Shinde <akshay.shinde@vit.edu.in>
2021-08-24 20:11:36 +0530

    added extract_ips func

--- automate.sh
+++ automate.sh
@@ -32,14 +32,14 @@ install_tools(){
 
 wrapper_for_files(){
 	cd $HOME
-	mkdir recon-files wordlist-making dnsgen-output censys-results altdns-output
+	mkdir ip-based recon-files wordlist-making dnsgen-output censys-results altdns-output
 	wget --quiet https://raw.githubusercontent.com/Voker2311/recon-scripts/main/combine.py -O $HOME/wordlist-making/combine.py
 }
 
 censys_api(){
 	cd $HOME/censys-results
-	API_KEY="api_key" # Change this
-	SECRET="secret_key" # Change this
+	API_KEY="5fcee053-56ad-44c1-bbc1-a903a74d29b5" # Change this
+	SECRET="umsD9exor1vSGwWDmM1bDR4SkkAhggqY" # Change this
 	curl -s -X POST https://search.censys.io/api/v1/search/certificates -u $API_KEY:$SECRET -d "{\"query\":\"$TARGET\"}" | jq .results[] | grep subject_dn | grep -oE "CN=.*" | awk -F\" '{print $1}' | awk -F\= '{print $2}' | grep -v "*" | sort -u | grep -i "$TARGET" > censys-out.txt
 	shuffledns -silent -d "$TARGET" -list censys-out.txt -r /opt/massdns/lists/resolvers.txt > resolved.txt
 }
@@ -84,10 +84,10 @@ dnsgen(){
 	cp $HOME/wordlist-making/resolved.txt 1.txt
 	cp $HOME/recon-files/resolved.txt 2.txt
 	cp $HOME/censys-results/resolved.txt 3.txt
-	cat * | sort -u > final.txt
+	cat * | sort -u | grep "$target" > final.txt
 	rm 1.txt 2.txt 3.txt
-	dnsgen final.txt > permutations.txt
-	resolve_subs
+	#dnsgen final.txt > permutations.txt
+	#resolve_subs
 }
 
 altdns(){
@@ -95,10 +95,37 @@ altdns(){
 	cp $HOME/dnsgen-output/final.txt .
 	altdns -i final.txt -o permutations -w /opt/altdns/words.txt -r -s altdns_output.txt
 }
+
 resolve_subs(){
 	cd $HOME/dnsgen-output
 	/opt/massdns/bin/massdns -r /opt/massdns/lists/resolvers.txt -t A -q -o S permutations.txt > dnsgen-resolved.txt
 	cp dnsgen-resolved.txt $HOME/subs.txt
+}
+
+extract_ips(){
+	cd $HOME/ip-based
+	cp $HOME/dnsgen-output/final.txt .
+	cat final.txt | grep "$TARGET" > temp.txt
+	mv temp.txt final.txt
+	echo "[*] Domains names -> IP Addresses"
+	for i in `cat final.txt`;do
+		dig +short +recurse A $i;
+	done | grep -oE "[0-9]{1,3}.+" | grep -v "[a-z]" | sort -u > ips.txt
+	# Getting list of ips from censys.io
+	echo "[*] Getting the list of ips associated to the target from censys"
+	API_KEY="api_key" # Change this
+	SECRET="secret_key" # Change this
+	count=$(curl -s -u "$API_KEY":"$SECRET" -H 'Content-Type: application/json' "https://search.censys.io/api/v2/hosts/search?q=$TARGET&per_page=100" | jq -r .result.total)
+	iters=$(expr "$count" / 100 + 1)
+	cursor=""
+	for ((i = 1 ; i <= "$iters" ; i++));do
+		curl -s -u "$API_KEY":"$SECRET" -H 'Content-Type: application/json' "https://search.censys.io/api/v2/hosts/search?q=$TARGET&per_page=100&cursor=$cursor" | jq -r .result.hits[].ip >> ips.txt
+		next=$(curl -s -u "$API_KEY":"$SECRET" -H 'Content-Type: application/json' "https://search.censys.io/api/v2/hosts/search?q=$TARGET&per_page=100&cursor=$cursor" | jq -r .result.links.next)
+		if [[ ! -z "$next" ]];then
+			cursor=$next
+		fi			
+	done
+	cat ips.txt | sort -u > final-ips.txt
 }
 
 main(){
@@ -107,7 +134,6 @@ main(){
 	echo "[+] Scan started at $start_time"
 	#install_tools
 	echo "[*] Setting up the files"
-	ulimit -n 524288 # Set this limit as it will most likely encounter too many files open error
 	wrapper_for_files
 	echo "[*] Subdomain discovery.."
 	subdomain_discovery
@@ -117,12 +143,12 @@ main(){
 	bruteforce
 	echo "[*] Performing permutations on the subdomains found.."
 	dnsgen
-	echo "[*] Using altdns to generate words with dev,staging,etc"
-	altdns # Optional
+	#echo "[*] Using altdns to generate words with dev,staging,etc"
+	#altdns
 	end_time=$(date "+%T")
 	epoch2=$(date "+%s" -d $end_time)
 	tots=`expr $epoch2 - $epoch1`
 	echo "[+] Scan completed in "$tots" secs"
 }
 
-main
+extract_ips
