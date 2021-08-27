b0db5bce1d93a0a9592fd31dfa6366e15adbd1ce
Akshay Shinde <akshay.shinde@vit.edu.in>
2021-08-27 11:36:22 +0530

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -6,7 +6,7 @@ if [[ $# -eq 0 ]];then
         exit 1;                                                                                
 fi
 
-location="location_to_target_dir" # Change this
+location="target_dir" # Change this
 cd $location
 
 # Place automate.sh in target_dir
@@ -15,8 +15,18 @@ cd $location
 if [ ! -d $location/subslive ];then
        mkdir subslive
 fi
+ulimit -n 524288
+sleep 200
+# Perform dnsgen on final.txt
+cd $location/dnsgen-output
+dnsgen final.txt > subs1.txt
+dnsgen --wordlist /opt/altdns/words.txt final.txt > subs2.txt
+cat subs1.txt subs2.txt | sort -u > total.txt
+shuffledns -silent -d $target -list total.txt -r /opt/massdns/lists/resolvers.txt > resolved.txt
+sleep 100
+mv resolved.txt final.txt
 
-cp $location/dnsgen-output/final.txt | anew subslive/subdomains.txt > $location/subslive/diff.txt
+cat $location/dnsgen-output/final.txt | anew $location/subslive/subdomains.txt > $location/subslive/diff.txt
 
 if [ -s $location/subslive/diff.txt ]; then
         notify -silent -data $location/subslive/diff.txt -bulk # Configure the provider according to your convinience
